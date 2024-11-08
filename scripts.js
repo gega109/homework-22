ef72004c805a5a395d1571e06326e11889770e4b
gega109 <bakuravar1@gmail.com>
2024-11-08 12:17:27 +0400

    homework 22

---
+++ scripts.js
@@ -0,0 +1,142 @@
+// hm1
+// let numbers = [2, 4, 6, 8];
+
+// numbers.forEach((number) => {
+//   console.log(number * number);
+// });
+
+// hm2
+// let fruits = ["apple", "pineapple", "peach", "orange"];
+
+// fruits.forEach((fruit) => {
+//   console.log(fruit);
+// })
+// hm3
+// let numbers = [2, 4, 6, 8];
+// let sum = 0;
+// numbers.forEach((number) => {
+//   sum += number;
+// });
+// console.log(sum);
+
+// hm4
+// let students = ["ვახო", "გეგა", "ანდრია", "გუჯა"];
+
+// students.forEach((student, index) => {
+//   console.log(`სტუდენტი ${student} ${index + 1} ადგილზეა`);
+// });
+// hm 5
+// let numbers = [10, 13, 15, 18, 20];
+
+// numbers.forEach((number) => {
+//   if (number % 2 === 0) {
+//     console.log(number);
+//   }
+// });
+
+// hm6
+// let numbers = [2, 4, 6, 8];
+// let NewNumber = numbers.map((number) => {
+//   return number * number;
+// });
+// console.log(NewNumber);
+
+// hm7
+// let students = ["VAXO", "GEGA", "GUJA", "ANDRIA"];
+
+// let lowerCaseNames = students.map((student) => student.toLowerCase());
+// console.log(lowerCaseNames);
+
+// hm8
+// let numbers = [1, 2, 3, 4, 5];
+// let NewNumber = numbers.map((number) => number * 10);
+// console.log(NewNumber);
+
+// hm 9
+
+// let students = [
+//   { name: "vaxo", grade: 100 },
+//   { name: "gega", grade: 17 },
+//   { name: "niki", grade: 6 },
+//   { name: "mariami", grade: 12 },
+// ];
+// let studentInfo = students.map((student) => {
+//   console.log(student.name);
+// });
+
+// hm10
+// let number = [2, 5, 7, 8, 10];
+
+// newNumbers = number.map((number) => {
+//   if (number % 2 === 0) {
+//     console.log(number * number);
+//   } else {
+//     console.log(number);
+//   }
+// });
+
+// hm11
+// let numbers = [1, 2, 3, 4, 5];
+// let evenNumbers = numbers.filter((number) => {
+//   if (number % 2 === 0) {
+//     console.log(number);
+//   }
+// });
+// hm12
+// let names = ["gega", "jemali", "vaxo"];
+
+// let NewNames = names.filter((name) => name.length > 5)
+// console.log(NewNames);
+
+// hm14
+// let products = [
+//   { name: "vashli", price: 76 },
+//   { name: "atami", price: 140 },
+//   { name: "msxali", price: 120 },
+//   { name: "fortoxali", price: 52 },
+// ];
+// let NewProducts = products.filter((product) => product.price > 100);
+// console.log(NewProducts);
+
+// hm15
+// let numbers = [15, 25, 60, 70, 45];
+
+// let NewNumbers = numbers.filter((number) => number > 50);
+// console.log(NewNumbers);
+
+// hm13
+// let numbers = [15, -13, 12, -16];
+
+// let NewNumbers = numbers.filter((number) => number > 0);
+// console.log(NewNumbers);
+
+// hm16
+// let number = 1;
+// while (number <= 10) {
+//   console.log(number);
+//   number++;
+// }
+
+// hm17
+// let numbers = [1, 5, 7, 8, 5];
+// let SumNumber = 0;
+// let index = 0;
+
+// while (index < numbers.length) {
+//   SumNumber += numbers[index];
+//   index++;
+// }
+// console.log(SumNumber);
+// hm 18
+// let number = 1;
+// while (number <= 20) {
+//   console.log(number);
+//   number += 2;
+// }
+
+// hm19
+// let number = 20;
+// while (number >= 10) {
+//   console.log(number);
+//   number--;
+// }
